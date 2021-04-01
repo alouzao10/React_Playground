af6244197c46078b966a8c432f3474ac389a0fce
Alex Louzao <alouzao10@gmail.com>
2021-03-31 20:44:15 -0400

    initial commit of redux crash course

---
+++ redux_crash_course/src/Components/PostForm.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class PostForm extends Component {
+   render() {
+      return (
+         <div>
+            <h1>Post Form</h1>
+         </div>
+      );
+   }
+}
